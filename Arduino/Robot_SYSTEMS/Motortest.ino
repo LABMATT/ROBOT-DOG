a795ea1e4962a23d2d8ddcf16d25538e0810b8c3
LABMATT <labmattcontact@gmail.com>
2020-07-23 13:09:57 +1000

    Added Pot support
    Pot pins added and tested

--- Arduino/Robot_SYSTEMS/Motortest.ino
+++ Arduino/Robot_SYSTEMS/Motortest.ino
@@ -7,6 +7,13 @@ void LEG1verification()
 
   digitalWrite(L1An, HIGH);
   digitalWrite(L1Bn, HIGH);
+
+  pinMode(L1Apot, OUTPUT);
+  pinMode(L1Bpot, OUTPUT);
+  pinMode(L1Cpot, OUTPUT);
+  //digitalWrite(L1Apot, HIGH);
+  //digitalWrite(L1Bpot, HIGH);
+  digitalWrite(L1Cpot, HIGH);
 }
 
 void LEG2verification()
@@ -18,17 +25,31 @@ void LEG2verification()
 
   digitalWrite(L2An, HIGH);
   digitalWrite(L2Bn, HIGH);
+
+  pinMode(L2Apot, OUTPUT);
+  pinMode(L2Bpot, OUTPUT);
+  pinMode(L2Cpot, OUTPUT);
+  //digitalWrite(L2Apot, HIGH);
+  //digitalWrite(L2Bpot, HIGH);
+  //digitalWrite(L2Cpot, HIGH);
 }
 
 void LEG3verification()
 {
   digitalWrite(L3enable, HIGH);
   
-  //analogWrite(L3Apwm, 60);  
+  analogWrite(L3Apwm, 60);  
   analogWrite(L3Bpwm, 60);
 
   //digitalWrite(L3An, HIGH);
   digitalWrite(L3Bn, HIGH);
+
+  pinMode(L3Apot, OUTPUT);
+  pinMode(L3Bpot, OUTPUT);
+  pinMode(L3Cpot, OUTPUT);
+  //digitalWrite(L3Apot, HIGH);
+  //digitalWrite(L3Bpot, HIGH);
+  //digitalWrite(L3Cpot, HIGH);
 }
 
 void LEG4verification()
@@ -40,4 +61,11 @@ void LEG4verification()
 
   //digitalWrite(L4An, HIGH);
   //digitalWrite(L4Bn, HIGH);
+
+  pinMode(L4Apot, OUTPUT);
+  pinMode(L4Bpot, OUTPUT);
+  pinMode(L4Cpot, OUTPUT);
+  //digitalWrite(L4Apot, HIGH);
+  //digitalWrite(L4Bpot, HIGH);
+  //digitalWrite(L4Cpot, HIGH);
 }
--- Arduino/Robot_SYSTEMS/Robot_SYSTEMS.ino
+++ Arduino/Robot_SYSTEMS/Robot_SYSTEMS.ino
@@ -21,6 +21,10 @@ int L_1C;
 #define L1Bpwm 7
 #define L1Bn 30
 
+#define L1Apot A1
+#define L1Bpot A2
+#define L1Cpot A3
+
 float L_1_Lenth;
 Servo servo_Leg_1;
 
@@ -37,6 +41,11 @@ int L_2C;
 #define L2Bpwm 9
 #define L2Bn 28
 
+
+#define L2Apot A4
+#define L2Bpot A5
+#define L2Cpot A6
+
 float L_2_Lenth;
 Servo servo_Leg_2;
 
@@ -52,6 +61,11 @@ int L_3C;
 #define L3Bpwm 11
 #define L3Bn 26
 
+
+#define L3Apot A7
+#define L3Bpot A8
+#define L3Cpot A9 
+
 float L_3_Lenth;
 Servo servo_Leg_3;
 
@@ -62,11 +76,16 @@ int L_4C;
 
 #define L4enable 43
 
-#define L4Appwm 12
+#define L4Apwm 12
 #define L4An 25
 
-#define L4Bppwm 13
+#define L4Bpwm 13
 #define L4Bn 24
+
+
+#define L4Apot A10
+#define L4Bpot A11
+#define L4Cpot A12
 
 float L_4_Lenth = 3;
 Servo servo_Leg_4;
@@ -86,18 +105,36 @@ pinMode(L1Apwm, OUTPUT);
 pinMode(L1Bn , OUTPUT);
 pinMode(L1Bpwm, OUTPUT);
 pinMode(L1Bn, OUTPUT);
+pinMode(L1Apot, INPUT);
+pinMode(L1Bpot, INPUT);
+pinMode(L1Cpot, INPUT);
 
 pinMode(L2enable, OUTPUT);
 pinMode(L2Apwm, OUTPUT);
 pinMode(L2Bn, OUTPUT);
 pinMode(L2Bpwm, OUTPUT);
 pinMode(L2Bn, OUTPUT);
+pinMode(L2Apot, INPUT);
+pinMode(L2Bpot, INPUT);
+pinMode(L2Cpot, INPUT);
 
 pinMode(L3enable, OUTPUT);
 pinMode(L3Apwm, OUTPUT);
 pinMode(L3Bn, OUTPUT);
 pinMode(L3Bpwm, OUTPUT);
 pinMode(L3Bn, OUTPUT);
+pinMode(L3Apot, INPUT);
+pinMode(L3Bpot, INPUT);
+pinMode(L3Cpot, INPUT);
+
+pinMode(L4enable, OUTPUT);
+pinMode(L4Apwm, OUTPUT);
+pinMode(L4Bn , OUTPUT);
+pinMode(L4Bpwm, OUTPUT);
+pinMode(L4Bn, OUTPUT);
+pinMode(L4Apot, INPUT);
+pinMode(L4Bpot, INPUT);
+pinMode(L4Cpot, INPUT);
 
 Serial.begin(9600);
 
@@ -114,9 +151,8 @@ Serial.println("working");
 
 void testmotor()
 {
- //LEG1verification();
+ LEG1verification();
  LEG3verification();
-
- // LEG2verification();
-  // LEG4verification();
+ LEG2verification();
+ LEG4verification();
 }
